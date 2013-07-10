b6e22ae73b59b7753e84b5bd8e689b3428cabd4b
MightyPixel <ognyan_angelov@yahoo.com>
2013-07-10 19:42:06 +0000

    index content -> root | method fix

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  var content = fs.readFileToSync('index.html');
+  var content = fs.readFileSync('index.html');
   response.send(content.toStrint());
 });
 
